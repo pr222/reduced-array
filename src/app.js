6b2d8e05b183c80539552d5b37f5dc94abcdba89
mtslck <mats.loock@lnu.se>
2020-06-17 15:53:24 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+/**
+ * The starting point of the application.
+ *
+ * @author Johan Leitet <johan.leitet@lnu.se>
+ * @author Mats Loock <mats.loock@lnu.se>
+ * @version 1.0.0
+ */
+
+import { getSum } from './arrayExtension.js'
+
+const result = getSum([1, 2, 3])
+
+console.log(`${result} [90m=> [32m6[39m`)
---
+++ src/arrayExtension.js
@@ -0,0 +1,23 @@
+/**
+ * renameMe module.
+ *
+ * @module src/renameMe
+ * @author Johan Leitet <johan.leitet@lnu.se>
+ * @author Mats Loock <mats.loock@lnu.se>
+ * @author // TODO: YOUR NAME <YOUR EMAIL>
+ * @version 1.0.0
+ */
+
+// ------------------------------------------------------------------------------
+//  Public interface
+// ------------------------------------------------------------------------------
+
+/**
+ * Takes an array of numbers and returns the sum of all numbers in the array.
+ *
+ * @param {number[]} data - An array with numbers.
+ * @returns {number} The total sum of the numbers in the array.
+ */
+export function getSum (data) {
+  // TODO: Write your code here.
+}
